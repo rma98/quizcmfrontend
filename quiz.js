2a26e79799cbaf469a037c6463a0a614b331d9a1
Robson <robalbuquerque98@gmail.com>
2024-09-12 17:34:56 -0300

    Add quiz application with HTML, CSS, and JavaScript files

---
+++ quiz.js
@@ -0,0 +1,282 @@
+const quizData = {
+    1: [
+        // Etapa 1 - 20 Perguntas
+        { id: 1, question: "Jesus nasceu em qual cidade?", options: ["Nazaré", "Belém do Pará", "Georgia", "Belém"], answer: 3, time: 15 },
+        { id: 2, question: "Qual é o nome dos Pais da Virgem Maria?", options: ["José e Maria", "Madalena e João", "Joaquim e Ana", "Joaquim e Madalena"], answer: 2, time: 20 },
+        { id: 3, question: "Qual é o nome do primeiro Papa?", options: ["André", "Jesus", "João", "Pedro"], answer: 3, time: 15 },
+        { id: 4, question: "O que João Batista era de Jesus?", options: ["Primo", "Tio", "Pai", "Padastro"], answer: 0, time: 15 },
+        { id: 5, question: "Nome do anjo da Anunciação?", options: ["Miguel", "Rafael", "Samuel", "Gabriel"], answer: 3, time: 15 },
+        { id: 6, question: "Depois do anuncio do Anjo o que Maria fez?", options: ["Pulou corda", "Visitou sua prima", "Foi fazer almoço", "Foi varrer a casa"], answer: 1, time: 20 },
+        { id: 7, question: "Qual é o nome da pousada que Jesus Nasceu?", options: ["O Castelo", "Hotel Dumont", "Estábulo", "Pousada Belém"], answer: 2, time: 15 },
+        { id: 8, question: "Quando Jesus foi apresentado no templo o que aconteceu com Nossa Senhora?", options: ["Purificação", "Virou Mãe", "Tirou RG", "Adiciounou seu nome na Torá"], answer: 0, time: 20 },
+        { id: 9, question: "Qual é o nome do Rio que Jesus se Batizou?", options: ["Amazonas", "Jordão", "Amaraji", "Pelotas"], answer: 1, time: 15 },
+        { id: 10, question: "O que Maria fez para Jesus transformar Água em vinho?", options: ["Mandou", "Reclamou", "Intercedeu", "Obrigou"], answer: 2, time: 10 },
+        { id: 11, question: "Quando Jesus iniciou sua vida pública o que ele fez?", options: ["Buscou fama", "Ficou contando histórias", "Reescreveu a história", "Anunciou o Reino de Deus"], answer: 3, time: 15 },
+        { id: 12, question: "Quando Jesus ainda era homem e mostrou como divino o que aconteceu?", options: ["Transfigurou", "Virou Power Ranger", "Evoluiu", "Subiu de Nivél"], answer: 0, time: 15 },
+        { id: 13, question: "Qual é o nome do discípulo amado?", options: ["Pedro", "João", "João Batista", "Mateus"], answer: 1, time: 10 },
+        { id: 14, question: "O que significa Totus Tuus Mariae", options: ["Toda Terra tem Maria", "Toda Criança Mariana", "Tem Terra Mariana", "Todo Teu Maria"], answer: 3, time: 10 },
+        { id: 15, question: "Quantos anos tem a Colo de Mãe?", options: ["Vai fazer 1 ano", "Tem 1 ano", "Tem 2 anos", "Tem 3 anos"], answer: 1, time: 10 },
+        { id: 16, question: "O que significa ser assunta ao Céu?", options: ["Ter uma cadeia no Céu", "Ser levada ao Céu", "Subir ao Céu", "Ter assuntos no Céu"], answer: 1, time: 15 },
+        { id: 17, question: "Com quantos anos Jesus iniciou sua vida pública?", options: ["22", "25", "38", "33"], answer: 3, time: 10 },
+        { id: 18, question: "Qual é o primeiro livro do Novo Testamento?", options: ["Marcos", "Mateus", "João", "Lucas"], answer: 1, time: 10 },
+        { id: 19, question: "Em qual cidade a Água virou Vinho?", options: ["Judeia", "Galileia", "Caná", "Nazaré"], answer: 2, time: 15 },
+        { id: 20, question: "Por qual amigo Jesus chorou", options: ["Filipe", "Judas", "João Batista", "Lázaro"], answer: 3, time: 10 },
+    ],
+    2: [
+        // Etapa 2 - 10 Perguntas
+        { id: 1, question: "Qual é o nome do encontro anual da CN para consagrados?", options: ["Filhos de Maria", "Ekballo", "Dia de Fátima", "Consagração Mariana"], answer: 1, time: 15 },
+        { id: 2, question: "Quem trocou de Nome e se tornou grande pregador da Palavra?", options: ["Pedro", "Mateus", "Paulo", "João Batista"], answer: 2, time: 15 },
+        { id: 3, question: "Qual é o nome dado aos primeiros 5 livros da Bíblia?", options: ["Pentateuco", "Pentagono", "Quilênio Bíblico", "Quinto Bíblico"], answer: 0, time: 10 },
+        { id: 4, question: "Marque a frase correta abaixo?", options: ["Jesus foi assunto ao Céu", "Jesus recussitou no segundo dia", "Jesus é coroado com espinhos na Glória", "Faça-se em mim segundo tua palavra"], answer: 3, time: 15 },
+        { id: 5, question: "Quem entregou o Sermão da Montanha", options: ["Pedro", "Jesus", "Tiago", "Paulo"], answer: 1, time: 10 },
+        { id: 6, question: "Que governador Romano condenou Jesus a morte?", options: ["Caifás", "Judas", "Pilatos", "Josué"], answer: 2, time: 15 },
+        { id: 7, question: "Qual é o nome do primeiro assassino?", options: ["Judas", "Cleofás", "Caim", "Lancelot"], answer: 2, time: 10 },
+        { id: 8, question: "Qual é o nome do Rei mais conhecido e amado de Israel?", options: ["José", "Imperador", "Pilatos", "Davi"], answer: 3, time: 10 },
+        { id: 9, question: "Qual é o nome do profeta que chorava?", options: ["Daniel", "Jeremias", "Golias", "Noé"], answer: 1, time: 10 },
+        { id: 10, question: "Do que se trata o Evangelho dos atos dos Apóstolos?", options: ["Reuniões apostólicas e discurssão da Torá", "Leitura das profecias e Avivamento", "Primeiros anos apostólicos e Leitura da Torá", "Primeiros anos da igreja e anuncio do Evangelho"], answer: 3, time: 15 },
+    ],
+    3: [
+        // Etapa 3 - 10 Perguntas
+        { id: 1, question: "O que construimos no Natal como Católicos celebrando tal momento?", options: ["Ceia de Natal", "Reunião Familiar", "Missa do galo", "Presépio"], answer: 3, time: 10 },
+        { id: 2, question: "Qual salmo afirma que se passarmos pela sombra da morte nada devemos temer?", options: ["20", "21", "22", "23"], answer: 2, time: 10 },
+        { id: 3, question: "Qual é o nome da reunião de Dogmas Católicos", options: ["Bíblia", "Torá", "Encíclia", "Catecismo"], answer: 3, time: 10 },
+        { id: 4, question: "Onde Jesus encontrou Zaqueu", options: ["Em casa", "No caminho", "Capadócia", "Árvore"], answer: 3, time: 6 },
+        { id: 5, question: "Qual pai quase matou seu filho na Bíblia?", options: ["Moisés", "Noé", "Abraão", "Davi"], answer: 2, time: 6 },
+        { id: 6, question: "Quantas virgens tinham na Parábola de Jesus?", options: ["8", "10", "12", "14"], answer: 1, time: 6 },
+        { id: 7, question: "Miguel lutou contra que animal?", options: ["Bezerro de Ouro", "Leões", "Dragão", "Golias"], answer: 2, time: 10 },
+    ]
+};
+
+let currentQuestionIndex = 0;
+let score = 0;
+let username = "";
+let timer;
+let currentStep = 1;
+let completedUsers = [];
+let globalRankingStep1 = [];
+let globalRankingStep2 = [];
+
+// Função para Iniciar o Quiz
+function startQuiz() {
+    username = document.getElementById('username').value.trim(); // Remove espaços em branco
+    if (!username) {
+        alert('Por favor, insira seu nome.');
+        return;
+    }
+
+    // Carregar rankings do armazenamento local
+    loadRankings();
+
+    // Verificar se o nome já completou o quiz
+    if (completedUsers.some(player => player.name === username)) {
+        alert('Você já completou o quiz. Sua pontuação e posição são finais.');
+        return;
+    }
+
+    // Verificar se o nome já está no ranking da etapa atual
+    if (currentStep === 1 && globalRankingStep1.some(player => player.name === username)) {
+        alert('Nome já cadastrado no ranking da Etapa 1.');
+        return;
+    } else if (currentStep === 2 && globalRankingStep2.some(player => player.name === username)) {
+        alert('Nome já cadastrado no ranking da Etapa 2.');
+        return;
+    } else if (currentStep === 3 && completedUsers.some(player => player.name === username)) {
+        alert('Nome já cadastrado no ranking da Etapa 3.');
+        return;
+    }
+
+    document.getElementById('intro').style.display = 'none';
+    document.getElementById('quiz-container').style.display = 'block';
+    document.getElementById('ranking-container').style.display = 'none';
+    document.getElementById('final-result-container').style.display = 'none';
+    loadQuestion();
+}
+
+// Função para Carregar Questão
+function loadQuestion() {
+    const questionsPerStep = currentStep === 1 ? 20 : (currentStep === 2 ? 10 : 7);
+    const stepQuestions = quizData[currentStep];
+
+    if (currentQuestionIndex >= questionsPerStep) {
+        endStep();
+        return;
+    }
+
+    const questionData = stepQuestions[currentQuestionIndex];
+    document.getElementById('question').innerText = `Pergunta ${currentQuestionIndex + 1}: ${questionData.question}`;
+
+    const optionsContainer = document.getElementById('options-container');
+    optionsContainer.innerHTML = '';
+
+    questionData.options.forEach((option, index) => {
+        const button = document.createElement('button');
+        button.innerText = option;
+        button.onclick = () => selectAnswer(index);
+        optionsContainer.appendChild(button);
+    });
+
+    startTimer(questionData.time);
+}
+
+// Função para Iniciar o Timer
+function startTimer(seconds) {
+    clearInterval(timer);
+    document.getElementById('timer').innerText = `Tempo: ${seconds}s`;
+    timer = setInterval(() => {
+        seconds--;
+        document.getElementById('timer').innerText = `Tempo: ${seconds}s`;
+        if (seconds <= 0) {
+            clearInterval(timer);
+            currentQuestionIndex++;
+            loadQuestion();
+        }
+    }, 1000);
+}
+
+// Função para Selecionar a Resposta
+function selectAnswer(index) {
+    const questionData = quizData[currentStep][currentQuestionIndex];
+    if (index === questionData.answer) {
+        score += 0.5;
+    }
+    currentQuestionIndex++;
+    loadQuestion();
+}
+
+// Função para Encerrar a Etapa e Calcular Ranking
+function endStep() {
+    clearInterval(timer);
+
+    // Atualizar o ranking conforme a etapa
+    let ranking;
+    if (currentStep === 1) {
+        globalRankingStep1.push({ name: username, score: score });
+        ranking = globalRankingStep1;
+    } else if (currentStep === 2) {
+        globalRankingStep2.push({ name: username, score: score });
+        ranking = globalRankingStep2;
+    } else {
+        completedUsers.push({ name: username, score: score });
+        ranking = completedUsers;
+        saveCompletion(username); // Salva a conclusão do quiz
+    }
+
+    ranking.sort((a, b) => b.score - a.score);
+
+    saveRankings();
+
+    // Exibir o ranking da etapa atual
+    document.getElementById('quiz-container').style.display = 'none';
+    document.getElementById('ranking-container').style.display = 'block';
+
+    const tbody = document.querySelector('#ranking-table tbody');
+    tbody.innerHTML = '';
+
+    const topPlayers = currentStep === 1 ? 15 : (currentStep === 2 ? 7 : ranking.length);
+    ranking.slice(0, topPlayers).forEach((player, index) => {
+        const row = document.createElement('tr');
+        const nameCell = document.createElement('td');
+        const scoreCell = document.createElement('td');
+        const positionCell = document.createElement('td');
+
+        positionCell.innerText = index + 1;
+        nameCell.innerText = player.name;
+        scoreCell.innerText = player.score;
+
+        row.appendChild(positionCell);
+        row.appendChild(nameCell);
+        row.appendChild(scoreCell);
+        tbody.appendChild(row);
+    });
+
+    // Verificar condições para avançar
+    const playerIndex = ranking.findIndex(player => player.name === username);
+    const isEligible = (currentStep === 1 && score >= 5 && playerIndex < 15) || (currentStep === 2 && score >= 3 && playerIndex < 7);
+
+    if (isEligible) {
+        document.getElementById('nextStepButton').style.display = 'block';
+        document.getElementById('participation-message').style.display = 'none';
+    } else {
+        document.getElementById('participation-message').innerText = 'Obrigado por participar. Você não avançou para a próxima etapa.';
+        document.getElementById('participation-message').style.display = 'block';
+        document.getElementById('nextStepButton').style.display = 'none';
+    }
+
+    if (currentStep === 3) {
+        showFinalRanking();
+    }
+}
+
+// Função para Mostrar o Ranking Final
+function showFinalRanking() {
+    document.getElementById('final-result-container').style.display = 'block';
+    document.getElementById('ranking-container').style.display = 'none';
+
+    const finalRankingTable = document.querySelector('#final-ranking-table tbody');
+    finalRankingTable.innerHTML = '';
+
+    completedUsers.sort((a, b) => b.score - a.score).forEach((player, index) => {
+        const row = document.createElement('tr');
+        const nameCell = document.createElement('td');
+        const scoreCell = document.createElement('td');
+        const positionCell = document.createElement('td');
+
+        positionCell.innerText = index + 1;
+        nameCell.innerText = player.name;
+        scoreCell.innerText = player.score;
+
+        row.appendChild(positionCell);
+        row.appendChild(nameCell);
+        row.appendChild(scoreCell);
+        finalRankingTable.appendChild(row);
+    });
+
+    document.getElementById('final-score').innerText = `Sua Pontuação Final: ${score}`;
+    document.getElementById('final-message').innerText = 'Obrigado por completar o quiz! Aguardando o vencedor...';
+    document.getElementById('resetQuizButton').style.display = 'block';
+}
+
+// Função para Avançar para a Próxima Etapa
+function nextStep() {
+    currentStep++;
+    currentQuestionIndex = 0;
+    score = 0;
+    document.getElementById('quiz-container').style.display = 'block';
+    document.getElementById('ranking-container').style.display = 'none';
+    loadQuestion();
+}
+
+// Função para Voltar para a Tela Inicial
+function showIntro() {
+    // Limpar dados do quiz
+    currentStep = 1;
+    currentQuestionIndex = 0;
+    score = 0;
+    document.getElementById('intro').style.display = 'block';
+    document.getElementById('quiz-container').style.display = 'none';
+    document.getElementById('ranking-container').style.display = 'none';
+    document.getElementById('final-result-container').style.display = 'none';
+    document.getElementById('resetQuizButton').style.display = 'none'; // Esconde o botão de reinício
+}
+
+// Função para Salvar Conclusão do Quiz
+function saveCompletion(username) {
+    let completedUsers = JSON.parse(localStorage.getItem('completedUsers')) || [];
+    if (!completedUsers.some(player => player.name === username)) {
+        completedUsers.push({ name: username, score: score });
+        localStorage.setItem('completedUsers', JSON.stringify(completedUsers));
+    }
+}
+
+// Funções para Carregar e Salvar Rankings
+function loadRankings() {
+    globalRankingStep1 = JSON.parse(localStorage.getItem('globalRankingStep1')) || [];
+    globalRankingStep2 = JSON.parse(localStorage.getItem('globalRankingStep2')) || [];
+    completedUsers = JSON.parse(localStorage.getItem('completedUsers')) || [];
+}
+
+function saveRankings() {
+    localStorage.setItem('globalRankingStep1', JSON.stringify(globalRankingStep1));
+    localStorage.setItem('globalRankingStep2', JSON.stringify(globalRankingStep2));
+    localStorage.setItem('completedUsers', JSON.stringify(completedUsers));
+}
